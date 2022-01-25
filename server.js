1d3590e5381aa5a5a09362df6da6f2bf518d2c6d
เอกรัฐ สุขเขียว <ln_waut@hotmail.com>
2022-01-25 14:59:52 +0700

    gameitem commit

--- server.js
+++ server.js
@@ -1,47 +1,60 @@
-const { connect } = require('http2')
-var net = require('net')
-var readline = require('readline')
+var net = require('net');
+var HOST = '127.0.0.1';
+var PORT = 6969;
 
+var db = ["sword", "spear", "staff", "shield", "bow", "crossbow", "potion", "poison", "glove", "hat", "shoe", "apple"]
 
+net.createServer(function (sock) {
+    var state = 0
+    var current_key = null
+    var i = 0
+    sock.on('data', function (data) {
+        switch(state){
+            case 0:
+                if(data == 'STARTGAME'){
+                    sock.write('GAMESTART')
+                    state = 1 //wait for key
+                }
+                break
+            case 1:
+                current_key = data
+                //sock.write("" + (db[current_key] || 0))
+                for(i = 0 ; i<=db.length; i++) {
+                    if(db[i] == current_key) {
+                        if(i == 0){
+                            sock.write('' + (i+1))
+                        }else {
+                            sock.write('' + (i))
+                        }
+                    }
+                }
+                state = 2 //wait for number
+                break
+            case 2:
+                if(data == 'GAMEOVER!!'){
+                    sock.write('GG')
+                    sock.close()
+                    state = 3 //end
+                }else{
+                try{
+                    let v = data.toUppercase()
+                    if(!db[current_key]){
+                        db[current_key] = 0
+                        sock.write("" + (db[current_key]))
+                    //db[current_key] += v
+                    }
 
-var HOST = '127.0.0.1'
-var PORT = 6969
-
-var client = new net.Socket()
-client.connect(PORT, HOST, function() {
-    console.log('CONNECTED TO: ' + HOST + ':' + PORT)
-    //client.write('sword')
-});
-//var keydata = window.prompt(" Enter Item : ");
-//let data = prompt("Enter Item : ");
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
-client.on('r1', function(data) {
-    
-    
-    //const readline = require('readline');
-    //const data = readline.createInterface({
-    //   input : process.stdin,
-    //    output : process.stdout
-    //});
-
-    console.log('ServerGameData : ' + data)
-    sendline()
-    client.destroy()
-});
-
-client.on('close', function() {
-    console.log('GAMEOVER!')
-})
-
-function sendline() {
-    var line = r1.question('DataSent: ')
-    if(line == 'GAMEOVER!') {
-        client.end()
-    }else {
-        client.write(line)
-    }
-    r1.close();
-}
+                    for(i = 0; i < db.length; i++) {
+                        if(db[i] == v) {
+                            sock.write("" + (db[i] + 1))
+                        }
+                    }
+                }catch(e){
+                    sock.write('INVALID')
+                }
+            }
+            break
+        }
+    })
+}).listen(PORT, HOST);
+console.log('Server listening on ' + HOST + ':' + PORT);
